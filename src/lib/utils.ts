8db8dde2e85627d3969dd94c65aa09851b9f69ed
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-09-26 00:38:02 +1000

    feat: Add new utils to format date and strings

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,27 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(date: Date): string {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  return new Date(date).toLocaleDateString(undefined, options);
+}
+
+export function capitalise(str: string): string {
+  if (typeof str !== "string" || str.length === 0) {
+    return str;
+  }
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export function capitaliseAll(str: string): string {
+  return str.toUpperCase();
 }
