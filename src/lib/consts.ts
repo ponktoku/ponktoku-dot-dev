2b71ddaf0c996c80309831cce3703f01c4a1d87a
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-10-01 23:05:08 +1000

    refactor: Move worksEn and worksJa to consts file

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -16,3 +16,60 @@ export const NOT_TRANSLATED_CAUTION: string | Multilingual = {
   en: "This page is not available in your language.",
   ja: "このページはご利用の言語でご覧いただけません。",
 };
+
+// Works
+export const worksEn = [
+  {
+    title: "Wordwise",
+    description:
+      "University project to assess the validity of machine translation",
+    image: "@/assets/works/wordwise_01.png",
+    url: "/en/works/wordwise",
+    showLinkSvg: true,
+    workBadge: "uni project",
+  },
+  {
+    title: "ponktoku.dev",
+    description: "My first website that I put on the internet",
+    image: "@/assets/works/homepage_thumb.jpeg",
+    url: "/en/works/ponktoku-dev",
+    showLinkSvg: false,
+    workBadge: "personal project",
+  },
+  {
+    title: "ponktoku.dev.v2",
+    description: "It's the website you're seeing right now",
+    image: "/images/hool.png",
+    url: "/en/works/ponktoku-dev-v2",
+    showLinkSvg: false,
+    workBadge: "personal project",
+  },
+];
+
+export const worksJa = [
+  {
+    title: "ワードワイズ",
+    description:
+      "University project to assess the validity of machine translation",
+    image: "@/assets/works/wordwise_01.png",
+    url: "/en/works/wordwise",
+    showLinkSvg: true,
+    workBadge: "uni project",
+  },
+  {
+    title: "ponktoku.dev",
+    description: "My first website that I put on the internet",
+    image: "@/assets/works/homepage_thumb.jpeg",
+    url: "/en/works/ponktoku-dev",
+    showLinkSvg: false,
+    workBadge: "personal project",
+  },
+  {
+    title: "ponktoku.dev.v2",
+    description: "It's the website you're seeing right now",
+    image: "/images/hool.png",
+    url: "/en/works/ponktoku-dev-v2",
+    showLinkSvg: false,
+    workBadge: "personal project",
+  },
+];
