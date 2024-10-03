723ed01974b306d3efe2288123b64f4a4fe12325
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-10-03 16:05:59 +1000

    feat: Add text and icon links to footer

---
+++ src/components/icon-link.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Button } from "@/components/ui/button";
+import * as Icons from "./ui/icons";
+
+interface IconLinkProps {
+  iconName: keyof typeof Icons;
+  tooltipText: string;
+  link: string;
+  openNewTab?: boolean;
+}
+
+export default function IconLink({
+  iconName,
+  tooltipText,
+  link,
+  openNewTab,
+}: IconLinkProps) {
+  const [Icon, setIcon] = useState<React.ComponentType | null>(null);
+
+  useEffect(() => {
+    setIcon(() => Icons[iconName]);
+  }, [iconName]);
+
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <a href={link} {...(openNewTab ? { target: "_blank" } : {})}>
+            <Button variant="ghost" size="icon">
+              {Icon && <Icon />}
+            </Button>
+          </a>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>{tooltipText}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
