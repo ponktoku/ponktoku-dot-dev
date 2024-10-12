239eb67fe5c2a0a200af274b5f4b12a222d4e879
Danny Bao <88077806+DannyBoy5930@users.noreply.github.com>
2024-10-12 13:33:42 +1100

    chore: Update worksJa and blog post date

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -49,26 +49,25 @@ export const worksEn = [
 export const worksJa = [
   {
     title: "ワードワイズ",
-    description:
-      "University project to assess the validity of machine translation",
+    description: "大学のプロジェクトで機械翻訳の妥当性を評価する",
     image: "@/assets/works/wordwise_01.png",
-    url: "/en/works/wordwise",
+    url: "/ja/works/wordwise",
     showLinkSvg: true,
     workBadge: "uni project",
   },
   {
     title: "ponktoku.dev",
-    description: "My first website that I put on the internet",
+    description: "初めてインターネットに公開したウェブサイト",
     image: "@/assets/works/homepage_thumb.jpeg",
-    url: "/en/works/ponktoku-dev",
+    url: "ponktoku-dev",
     showLinkSvg: false,
     workBadge: "personal project",
   },
   {
     title: "ponktoku.dev.v2",
-    description: "It's the website you're seeing right now",
+    description: "あなたが見ているウェブサイト",
     image: "/images/hool.png",
-    url: "/en/works/ponktoku-dev-v2",
+    url: "/ja/works/ponktoku-dev-v2",
     showLinkSvg: false,
     workBadge: "personal project",
   },
